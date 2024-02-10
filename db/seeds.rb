dcc3f19c09d6c18a4fa5a08de4516f7cd750843b
Samuel Anderson <samuelSD.999@gmail.com>
2024-02-10 14:46:49 +0000

    Seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ Movie.create(title: 'Wonder Woman 1984',
              poster_url: 'https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg',
              rating: 6.9)
 Movie.create(title: 'The Shawshank Redemption',
-             overview: 'Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison',
+             overview: 'Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life atthe',
              poster_url: 'https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg',
              rating: 8.7)
 Movie.create(title: 'Titanic',
@@ -21,6 +21,6 @@ Movie.create(title: 'Titanic',
              poster_url: 'https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg',
              rating: 7.9)
 Movie.create(title: "Ocean's Eight",
-             overview: 'Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.',
+             overview: 'Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of',
              poster_url: 'https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg',
              rating: 7.0)
